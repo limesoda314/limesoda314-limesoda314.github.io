fca9b410c467192227ac4d33a3033732785a7758
limesoda314 <limesoda314@gmail.com>
2020-11-06 00:01:48 -0800

    iframe issues
    I think I fixed how the iframe doesn't fully appear on mobile devices (changed min-height of the container) but I'm not really sure that actually works. I also found a new problem where the menu button doesn't work when it's over an iframe :(

--- javascript/theme.js
+++ javascript/theme.js
@@ -18,3 +18,16 @@ function
   // Get the element with id="defaultOpen" and click on it
   document.getElementById("defaultOpen").click();
 
+/**
+ * Resizes the given iFrame width so it fits its content
+ * @param e The iframe to resize
+ */
+function resizeIframeWidth(e){
+  // Set width of iframe according to its content
+  if (e.Document && e.Document.body.scrollWidth) //ie5+ syntax
+      e.width = e.contentWindow.document.body.scrollWidth;
+  else if (e.contentDocument && e.contentDocument.body.scrollWidth) //ns6+ & opera syntax
+      e.width = e.contentDocument.body.scrollWidth + 35;
+  else (e.contentDocument && e.contentDocument.body.offsetWidth) //standards compliant syntax – ie8
+      e.width = e.contentDocument.body.offsetWidth + 35;
+}
