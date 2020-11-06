4c9145b20b5a45c2d46423bbf1e2fb429fa5adb1
limesoda314 <limesoda314@gmail.com>
2020-11-05 22:41:15 -0800

    Cleaned up the site a bit
    I tried reorganizing the syle and js sheets as well as fixing the iframes again :( I'm going to try changing the background again, but I don't know how well that will go.

--- javascript/theme.js
+++ javascript/theme.js
@@ -1,39 +1,3 @@
-
-  // SNACK BARRRRRR
- function myFunction() {
-  // Get the snackbar DIV
-  var x = document.getElementById("snackbar");
-
-  // Add the "show" class to DIV
-  x.className = "show";
-
-  // After 3 seconds, remove the show class from DIV
-  setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
-}  
-
-// IMAGE THINGY 
-// Get the modal
-var modal = document.getElementById("myModal");
-
-// Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById("myImg");
-var modalImg = document.getElementById("img01");
-var captionText = document.getElementById("caption");
-img.onclick = function(){
-  modal.style.display = "block";
-  modalImg.src = this.src;
-  captionText.innerHTML = this.alt;
-}
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-
 function 
   openPage(pageName, elmnt, color) { 
   // Hide all elements with class="tabcontent" by default */  
